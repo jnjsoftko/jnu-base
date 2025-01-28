32a84ef880291ecabcb9d3d3e4c86eca38fba8e6
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2025-01-28 10:07:17 +0900

    chore: build for publish

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # [syntax] ./publish.sh patch|minor|major
 # default: patch
 
@@ -5,14 +6,12 @@ mode=${1:-patch}
 
 # 1. 빌드
 yarn clean:mac && yarn build && \
-
-# 2. npm 버전 업데이트
-npm version $mode && \
-
-# 3. git 변경사항 커밋 및 푸시
+# 2. git 변경사항 커밋
 git add . && \
 git commit -m "chore: build for publish" && \
+# 3. npm 버전 업데이트 (이때 자동으로 버전 태그가 생성됨)
+npm version $mode && \
+# 4. git push
 git push --follow-tags && \
-
-# 4. npm 배포
+# 5. npm 배포
 npm publish
